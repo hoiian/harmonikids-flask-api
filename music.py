6afcb9b7b36efc2bf5e01b53673dd56f62588d1a
hoiian <hoiian96@gmail.com>
2025-04-10 06:01:44 +0800

    update public port

--- music.py
+++ music.py
@@ -233,4 +233,5 @@ def reset():
     return jsonify({"status": "ok", "message": "已重置所有音符"})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
